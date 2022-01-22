20bf8ae48c22327bea22b5f4394e6dc8ba87f4c3
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-01-22 11:53:20 +0200

    Added interface IList

---
+++ PRsExample/IList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRsExample
+{
+    public interface IList<T>
+    {
+        void Add(T element);
+        void AddFront(T element);
+        void AddByIndex(int index, T element);
+    }
+}
