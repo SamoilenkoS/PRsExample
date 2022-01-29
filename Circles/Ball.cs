913f42bff322460a20e71e0d5401c05870e46956
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-01-29 10:16:39 +0200

    Added IRandom example

--- Circles/Ball.cs
+++ Circles/Ball.cs
@@ -7,30 +7,41 @@ namespace Circles
 {
     public class Ball
     {
-        private static Random random;
+        private IRandom random;
         public Point Position { get; set; }
         public int XSpeed { get; set; }
         public int YSpeed { get; set; }
         public int Size { get; set; }
         public Point RadiusCoordinates => new Point(Position.X + Size / 2, Position.Y + Size / 2);
 
-        static Ball()
-        {
-            random = new Random();
-        }
-
         public Ball(int x = 0, int y = 0)
         {
+            random = new BaseRandom();
             Position = new Point { X = x, Y = y };
-            XSpeed = random.Next(5, 20);
-            YSpeed = random.Next(5, 20);
-            Size = random.Next(20, 100);
+            XSpeed = random.GetRandom(5, 20);
+            YSpeed = random.GetRandom(5, 20);
+            Size = random.GetRandom(20, 100);
         }
 
         public void ReverseDirection()
         {
             XSpeed *= -1;
             YSpeed *= -1;
+        }
+
+        private class BaseRandom : IRandom
+        {
+            private static Random _random;
+
+            static BaseRandom()
+            {
+                _random = new Random();
+            }
+
+            public int GetRandom(int min, int max)
+            {
+                return _random.Next(min, max);
+            }
         }
     }
 }
